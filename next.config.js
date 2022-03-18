d2b38fe14e4d8541228f4aca7c0766ac412b7642
washington-shoji <shoji.washington@gmail.com>
2022-03-18 23:08:15 +1100

    feat(config): Updated the next.js config file

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 
-
 module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: [ 'localhost:3000', 'img.evbuc.com', ],
-  }
+	reactStrictMode: true,
+	i18n: {
+		locales: ['en'],
+		defaultLocale: 'en',
+	},
+	images: {
+		domains: ['localhost:3000', 'img.evbuc.com'],
+	},
 };
